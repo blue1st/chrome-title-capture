ea9665b3039ee49a98a73037ac9eb8d6c672a287
blue1st <iwannabeabat@gmail.com>
2016-12-14 16:21:49 +0900

    fix bug

--- setting.js
+++ setting.js
@@ -1,5 +1,5 @@
 function load_setting() {
-    return localStorage["pattern"] ? localStorage["pattern"] : '<a href="${url}"><image src="${favIconUrl}"></image>${title}</a>'
+    return localStorage["pattern"] ? localStorage["pattern"] : '<a href="${url}"><img src="${favIconUrl}"></img>${title}</a>'
 }
 
 function save_setting(pattern) {
